06aef4b5cd2a90044a5376f36df10c09650b0367
JaykumarPatel4802 <patel.jay4802@gmail.com>
2025-02-27 11:33:31 -0600

    add a SQLiteHelper to un the generated SQL queries

---
+++ code/SQLiteHelper.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+class SQLiteHelper:
+
+    def __init__(self):
+        self.conn = sqlite3.connect("database/Chinook_Sqlite.sqlite")
+        self.cur = self.conn.cursor()
+
+    def __del__(self):
+        self.conn.close()
+
+    def executeQuery(self, query):
+        try:
+            self.cur.execute(query)
+            rows = self.cur.fetchall()
+            return rows, None  # Return rows and None for no error
+        except sqlite3.Error as e:
+            return None, str(e)  # Return None for rows and the error message
--- code/main.py
+++ code/main.py
@@ -1,3 +1,14 @@
 from ChatGPT_Text2SQL import ChatGPT_Text2SQL
+from SQLiteHelper import SQLiteHelper
 gpt = ChatGPT_Text2SQL()
-print(gpt.pipeline("How many distinct artists and songs are there?"))
+query = gpt.pipeline("How many distinct artists are there and how many distinct songs are there?")
+
+print("Query:")
+print(query)
+
+sqlite_helper = SQLiteHelper()
+executionResult = sqlite_helper.executeQuery(query=query)
+print(executionResult)
+
+executionResult = sqlite_helper.executeQuery(query="SELECT COUNT(DISTINCT a.ArtistId) as DistinctArtists FROM Artist a;")
+print(executionResult)
