6947f5fc166aa961746cafb3eb2ea7faa2fae32f
JaykumarPatel4802 <patel.jay4802@gmail.com>
2025-02-27 11:41:50 -0600

    add agentless ability

--- code/ChatGPT_Text2SQL.py
+++ code/ChatGPT_Text2SQL.py
@@ -14,11 +14,12 @@ class GeneratedSQLQuery(BaseModel):
 
 class ChatGPT_Text2SQL(AgenticFramework):
 
-    def __init__(self):
+    def __init__(self, is_agentic=False):
         super().__init__()
         self.llm = init_chat_model("gpt-4o-mini", model_provider="openai")
         self.sh = SchemaHelper()
         self.sh.store_schemas()
+        self.is_agentic = is_agentic
 
     # Create agent to retrieve number of tables that are relevant to the query
     def retrieve_number_of_tables(self, query: str) -> int:
@@ -35,11 +36,17 @@ class ChatGPT_Text2SQL(AgenticFramework):
         return response.number_of_tables
 
 
-    # Create agent to retrieve the schemas of the tables
-    def retrieve_schemas(self, query: str, contextCount: int) -> str:
+    # Create agent to retrieve the relevant schemas of the tables
+    def retrieve_relevant_schemas(self, query: str, contextCount: int) -> str:
         relevantSchemas = self.sh.retrieve_relevant_schemas(query, contextCount)
         joinedRelevantSchemas = "\n\n----------\n\n".join(relevantSchemas)
         return joinedRelevantSchemas
+    
+    # Create agent to retrieve al schemas of the tables
+    def retrieve_all_schemas(self) -> str:
+        relevantSchemas = self.sh.retrieve_all_schemas()
+        joinedAllSchemas = "\n\n----------\n\n".join(relevantSchemas)
+        return joinedAllSchemas
 
     # Create agent to use the schema to generate a SQL query given a natural language query
     def generate_sql_query(self, query: str, relevant_schemas: str) -> str:
@@ -59,9 +66,13 @@ class ChatGPT_Text2SQL(AgenticFramework):
         pass
 
     def pipeline(self, query: str) -> str:
-        numRelevantTables = self.retrieve_number_of_tables(query)
-        retrievedSchemas = self.retrieve_schemas(query, numRelevantTables)
-        generatedSQLQuery = self.generate_sql_query(query, retrievedSchemas)
+        if self.is_agentic:
+            numRelevantTables = self.retrieve_number_of_tables(query)
+            retrievedSchemas = self.retrieve_relevant_schemas(query, numRelevantTables)
+            generatedSQLQuery = self.generate_sql_query(query, retrievedSchemas)
+        else:
+            allSchemas = self.retrieve_all_schemas()
+            generatedSQLQuery = self.generate_sql_query(query, allSchemas)
+        
         self.save_sql_query(query, generatedSQLQuery)
-
         return generatedSQLQuery
